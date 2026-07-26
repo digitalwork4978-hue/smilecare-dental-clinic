7c349703745244787a14f6b35c985d899d8de47f
digitalwork4978-hue <digitalwork4978@gmail.com>
2026-07-26 23:54:03 +0530

    Test Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
   return (
-    <div style={{ padding: "40px" }}>
-      <h1>✅ SmileCare Website Working</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }

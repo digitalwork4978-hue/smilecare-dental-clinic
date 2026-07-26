4e0306128b020d5c0f17d22761994128d2707f21
digitalwork4978-hue <digitalwork4978@gmail.com>
2026-07-26 23:58:11 +0530

    Test WhyChooseUs

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <WhyChooseUs />
     </>
   );
 }
